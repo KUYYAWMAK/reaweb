99ba599349b0839deead3912dd3255658d7c348b
KUYYAWMAK <jantimamarayat@gmail.com>
2026-02-15 22:19:34 +0700

    main

--- src/contents/experience.js
+++ src/contents/experience.js
@@ -1,46 +1,38 @@
-import {
-  faGithub,
-  faMedium,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
-import picExp from "../assets/exp_site.jpg";
-import picLittleLemon from "../assets/littlelemon.jpg";
-
+import { faGithub, faYoutube } from "@fortawesome/free-solid-svg-icons";
+import picExp from '../assets/Expic1.png'
 export const data = [
-  {
-    date: "2020-2021",
-    title: "Fullstackkk · Sport Analysis System",
-    link: "https://kuyyawmak.github.io/reaweb/",
-    materials: [
-      { type: faGithub, link: "https://kuyyawmak.github.io/reaweb/" },
-      { type: faYoutube, link: "https://kuyyawmak.github.io/reaweb/" },
-      { type: faMedium, link: "https://kuyyawmak.github.io/reaweb/" },
-    ],
-    descriptions: [
-      "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
-      "Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7.",
-    ],
-    skills: [
-      ["React", "Redus", "MUI"],
-      ["NginX", "NodeJS", "Express", "MongoDB"],
-    ],
-  },
-  {
-    date: "2019-2020",
-    title: "Fashion Ecommerce",
-    link: "https://kuyyawmak.github.io/reaweb/",
-    materials: [{ type: faGithub, link: "https://kuyyawmak.github.io/reaweb/" }],
-    descriptions: [
-      "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
-    ],
-    skills: [["NginX", "NodeJS", "Express", "MongoDB"]],
-  },
-  {
-    date: "2018-2019",
-    title: "Fashion Ecommerce",
-    descriptions: [
-      "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
-    ],
-    skills: [["NginX", "NodeJS", "Express", "MongoDB"]],
-  },
-];
+    {
+        date: "2024-202",
+        title: "Frontend Developer",
+        link: "https://github.com/suriyapi/react101",
+        material: [
+            {type: faGithub, link: "https://github.com/suriyapi/react101"},
+            {type: faYoutube, link: "https://www.youtube.com/@suriyapi"}
+        ],
+        descriptions: [
+            "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers."
+        ],
+        skills: [
+         ['React', 'Next'],
+         ['MUI']
+        ],
+        picture: picExp
+    },
+    {
+        date: "2024-2029",
+        title: "Frontend Developer",
+        link: "https://github.com/suriyapi/react101",
+        material: [
+            {type: faGithub, link: "https://github.com/suriyapi/react101"},
+            {type: faYoutube, link: "https://www.youtube.com/@suriyapi"}
+        ],
+        descriptions: [
+            "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers."
+        ],
+        skills: [
+         ['React', 'Next'],
+         ['MUI']
+        ],
+        picture: picExp
+    }
+]
--- src/contents/header.js
+++ src/contents/header.js
@@ -1,9 +1,9 @@
-import filename from '../assets/CV.pdf';
+import fileName from '../assets/cv.pdf';
 
-export const data = {
-name:"Thanaphat Maneechot",
-title:"Software Developer · between jobs",
-caption:"From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
-btnText:"View Resume",
-link:filename,
+export const data ={
+    "name": "Thanaphat Maneechot",
+    "title": "Software Developer · between jobs",
+    "caption": "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
+    btnText: "View Resume",
+    link: fileName
 }
